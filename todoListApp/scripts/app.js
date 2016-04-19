ef815fca28a69c4426bcd38f41a81def81bf0d1a
ajuchem <ajuchem@daudt.com>
2016-04-19 14:00:50 -0600

    Create another controller to understand scope

--- todoListApp/scripts/app.js
+++ todoListApp/scripts/app.js
@@ -1,6 +1,17 @@
 angular.module("todoListApp", [])
-.controller('mainCtrl', function($scope) {
-  $scope.helloWorld = function() {
+
+.controller('mainCtrl', function($scope){
+  $scope.helloWorld = function(){
     console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
+  };
+})
+
+.controller('coolCtrl', function($scope){
+  $scope.whoAmI = function(){
+    console.log('Hello there, this is coolCtrl function!');
+  };
+
+  $scope.helloWorld = function(){
+    console.log('This is not the main ctrl!');
   }
-});
+});;
