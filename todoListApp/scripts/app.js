462583f37b04a9f2d51f51851ab7a48890969900
ajuchem <ajuchem@daudt.com>
2016-04-19 17:26:38 -0600

    Use services to save and delete data

--- todoListApp/scripts/app.js
+++ todoListApp/scripts/app.js
@@ -12,13 +12,31 @@ angular.module("todoListApp", [])
     $scope.todos = response.data;
   });
 
+  $scope.deleteTodo = function(todo, $index){
+    dataService.deleteTodo(todo);
+    $scope.todos.splice($index, 1);
+  };
+
+  $scope.saveTodo = function(todo){
+    dataService.saveTodo(todo);
+  };
+
 })
 .service('dataService', function($http){
   this.helloWorld = function(){
-    console.log('This is the hello console service.');
+    console.log("This is the data's service method!");
   };
 
   this.getTodos = function(callback){
     $http.get('mock/todos.json').then(callback)
+  };
+
+  this.deleteTodo = function(todo){
+    console.log("The " + todo.name + " todo has been deleted.");
+    // other logic
+  };
+
+  this.saveTodo = function(todo) {
+    console.log("The " + todo.name + " todo has been saved.");
   }
 });
